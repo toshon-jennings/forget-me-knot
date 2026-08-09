71d746898831cd007eb01a157d4c486d356b887e
Toshon <toshon.tech@gmail.com>
2026-08-09 16:37:55 -0400

    Fix tray UI not showing on left click

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,9 +7,12 @@ use tauri::{
 };
 use tauri_plugin_autostart::MacosLauncher;
 
+use tauri_plugin_positioner::{WindowExt, Position};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             Some(vec![]),
@@ -30,30 +33,36 @@ pub fn run() {
             let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
+                .show_menu_on_left_click(false)
                 .on_menu_event(|_app: &tauri::AppHandle, event| match event.id().as_ref() {
                     "quit" => {
                         std::process::exit(0);
                     }
                     _ => {}
                 })
-                .on_tray_icon_event(|tray: &tauri::tray::TrayIcon, event| match event {
-                    TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        button_state: MouseButtonState::Up,
-                        ..
-                    } => {
-                        let app = tray.app_handle();
-                        if let Some(window) = app.get_webview_window("main") {
-                            let is_visible: bool = window.is_visible().unwrap_or(false);
-                            if is_visible {
-                                let _ = window.hide();
-                            } else {
-                                let _ = window.show();
-                                let _ = window.set_focus();
+                .on_tray_icon_event(|tray: &tauri::tray::TrayIcon, event| {
+                    let app = tray.app_handle();
+                    tauri_plugin_positioner::on_tray_event(app, &event);
+                    
+                    match event {
+                        TrayIconEvent::Click {
+                            button: MouseButton::Left,
+                            button_state: MouseButtonState::Up,
+                            ..
+                        } => {
+                            if let Some(window) = app.get_webview_window("main") {
+                                let is_visible: bool = window.is_visible().unwrap_or(false);
+                                if is_visible {
+                                    let _ = window.hide();
+                                } else {
+                                    let _ = window.move_window(Position::TrayCenter);
+                                    let _ = window.show();
+                                    let _ = window.set_focus();
+                                }
                             }
                         }
+                        _ => {}
                     }
-                    _ => {}
                 })
                 .build(app)?;
 
